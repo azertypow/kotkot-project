a5796eb5f4db5f9e9f36c437bfc0bc1537721635
azertypow <azertypow@gmail.com>
2017-04-12 02:27:11 +0200

    template et synchronisation mise en place

--- prod/browser/main.js
+++ prod/browser/main.js
@@ -99,7 +99,63 @@ exports.default = SocketClientApp;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 var socketClientApp_1 = __webpack_require__(0);
+var playerTemplate_1 = __webpack_require__(3);
 socketClientApp_1.default.run();
+var playerTemplate = new playerTemplate_1.default('1', 'pusher', 'attend ton tour pour jouer');
+var playerElementIndex = document.querySelector("#index");
+var playerElementStatus = document.querySelector("#status");
+var playerElementRules = document.querySelector("#rules");
+playerTemplate.setElements(playerElementIndex, playerElementStatus, playerElementRules);
+playerTemplate.initValues();
+var patern = {
+    index: 1,
+    status: "pusher",
+    rules: "attend les ordre que l'on va te dicter petite merde d'humain"
+};
+playerTemplate.setValues(patern);
+
+
+/***/ }),
+/* 2 */,
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var PlayerTemplate = (function () {
+    function PlayerTemplate(setIndex, setStatus, setRules) {
+        this.player_info = {
+            index: setIndex,
+            status: setStatus,
+            rules: setRules
+        };
+    }
+    PlayerTemplate.prototype.setElements = function (indexElement, statusElement, rulesElement) {
+        this.player_elements = {
+            index: indexElement,
+            status: statusElement,
+            rules: rulesElement
+        };
+    };
+    PlayerTemplate.prototype.initValues = function () {
+        this.player_elementsTemplate = {
+            index: this.player_elements.index.innerHTML,
+            status: this.player_elements.status.innerHTML,
+            rules: this.player_elements.rules.innerHTML
+        };
+    };
+    PlayerTemplate.prototype.setValues = function (patern) {
+        var renderIndex = Mustache.render(this.player_elementsTemplate.index, patern);
+        var renderStatus = Mustache.render(this.player_elementsTemplate.status, patern);
+        var renderRules = Mustache.render(this.player_elementsTemplate.rules, patern);
+        this.player_elements.index.innerHTML = renderIndex;
+        this.player_elements.status.innerHTML = renderStatus;
+        this.player_elements.rules.innerHTML = renderRules;
+    };
+    return PlayerTemplate;
+}());
+exports.default = PlayerTemplate;
 
 
 /***/ })
