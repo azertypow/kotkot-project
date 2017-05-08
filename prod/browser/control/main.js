c6594e077da49204089af39d7e8cf1a3228741de
azertypow <azertypow@gmail.com>
2017-05-08 17:49:29 +0200

    integration script js mathilde

--- prod/browser/control/main.js
+++ prod/browser/control/main.js
@@ -111,6 +111,7 @@ exports.default = LocationInfo;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 var locationInfo_1 = __webpack_require__(1);
+var socketEmitButton_1 = __webpack_require__(7);
 var socketControlApp = (function () {
     function socketControlApp() {
     }
@@ -132,6 +133,7 @@ var socketControlApp = (function () {
                 status: "haha"
             });
         });
+        socketEmitButton_1.default.run(socket);
     };
     return socketControlApp;
 }());
@@ -152,6 +154,91 @@ var socketControlApp_1 = __webpack_require__(2);
 var removeSleepMode_1 = __webpack_require__(0);
 removeSleepMode_1.default.run();
 socketControlApp_1.default.run();
+
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var SocketEmitButton = (function () {
+    function SocketEmitButton() {
+    }
+    SocketEmitButton.run = function (socket) {
+        var json = {
+            loisJ1: [
+                "Je te propose trois lois de Gauche",
+                "Je te propose deux lois de Gauche et une loi de Droite",
+                "Je te propose une loi Gauche et deux lois de Droite",
+                "Je te propose trois lois de Droite",
+                "Premier choix : veux-tu proposer une loi de Gauche ?",
+                "Premier choix : veux-tu proposer une loi de Droite ?",
+                "Deuxième choix : veux-tu proposer une loi de Gauche ?",
+                "Deuxième choix : veux-tu proposer une loi de Droite ?",
+                "Choix validé",
+                "Choix refusé, cette option n'est pas disponible"
+            ],
+            loisJ2: ["L'autre joueur te propose deux lois de Gauche",
+                "L'autre joueur te propose une loi de Gauche et une loi de Droite",
+                "L'autre joueur te propose deux lois de Droite",
+                "Veux-tu proposer une loi de Gauche ?",
+                "Veux-tu proposer une loi de Droite ?",
+                "Choix validé",
+                "Choix refusé, cette option n'est pas disponible"
+            ],
+            narration: [
+                "Bienvenue dans le jeu",
+                "Vous appartenez au Parti de Gauche. Votre but est de...",
+                "Vous appartenez au Parti de Droite. Votre but est de..."
+            ]
+        };
+        var singleControl = document.getElementsByClassName("control");
+        var propositions = document.getElementsByClassName("proposition");
+        for (var i = 0; i < singleControl.length; i++) {
+            singleControl[i].addEventListener('click', displayPropositions);
+        }
+        for (var j = 0; j < propositions.length; j++) {
+            propositions[j].addEventListener('click', sendProposition);
+        }
+        function sendProposition(e) {
+            console.log(e.target.textContent);
+        }
+        function displayPropositions(e) {
+            if ((e.target.classList[0]) !== "control") {
+                if ((e.target.classList[0]) === "proposition") {
+                    sendProposition(e);
+                    return;
+                }
+                else {
+                    console.log("wrong element - l'élément n'a pas la class 'control'");
+                    return;
+                }
+            }
+            var currentElement = e.target;
+            var currentClass = e.target.classList[1];
+            if (document.getElementById('proposition') !== null) {
+                var prop = document.getElementById('proposition');
+                prop.parentNode.removeChild(prop);
+            }
+            var propositions = document.createElement("div");
+            propositions.setAttribute("id", "proposition");
+            currentElement.appendChild(propositions);
+            for (var i = 0; i < json[currentClass].length; i++) {
+                var sentence = document.createElement("p");
+                sentence.setAttribute("class", "proposition" + " proposition-" + i);
+                sentence.textContent = json[currentClass][i];
+                propositions.appendChild(sentence);
+            }
+            socket.emit('control-click', {
+                name: "control"
+            });
+        }
+    };
+    return SocketEmitButton;
+}());
+exports.default = SocketEmitButton;
 
 
 /***/ })
