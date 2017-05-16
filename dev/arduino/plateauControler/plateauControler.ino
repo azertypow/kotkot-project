875c1f7b2d5f2a1d0b639dbc857a2895b9b18661
azertypow <azertypow@gmail.com>
2017-05-16 18:06:42 +0200

    connection arduino server par serialport et communication sur les potentiomettre

--- dev/arduino/plateauControler/plateauControler.ino
+++ dev/arduino/plateauControler/plateauControler.ino
@@ -2,25 +2,57 @@ int playersNumber = 2;
 
 void setup() {
   Serial.begin(250000);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 }
 
 void loop() {
   for(int i = 0; i < playersNumber; i++ ){
     String potenValu = readPotentiometreValue(i);
-    Serial.print( potenValu );
+
+    if(i == 0){
+      Serial.print("{"); // ouverture json
+    }
+
+    if(i < (playersNumber - 1)){
+      Serial.print( potenValu ); // valeur
+      Serial.print(","); // virgule pour valeur suivante
+    } else {
+      Serial.print( potenValu ); // valeur, pas de virgule
+      Serial.print("}"); // fermeture json
+    }
   }
 
-    Serial.print(" autre ");
+//  Serial.print(" autre ");
+//  String a = readPotentiometreValue(0);
+//  Serial.print( a );
 
-    String a = readPotentiometreValue(0);
-    Serial.print( a );
-
-    Serial.print("\r\n");
+  //  fin info
+  Serial.print("\r\n");
 }
 
 String readPotentiometreValue(int playerIndex){
-  String stringInfo = String(playerIndex)+": ";
+  String stringInfo = "\""+String(playerIndex)+"\": ";
   int analogPort = playerIndex;
-  String sensorValue = stringInfo + analogRead(analogPort) + " ";
+  String sensorValue = stringInfo + analogRead(analogPort);
   return sensorValue;
+}
+
+// lecture des info recu par le server
+String info = "";
+void serialEvent(){
+  int incomingByte = 0;   // for incoming serial data
+  incomingByte = Serial.read();
+  char incomingChar = incomingByte; // convert to char
+  if (String(incomingChar) == "\n"){
+    Serial.println("recu");
+    Serial.println(info);
+    info = "";
+  }
+  else {
+    String genere = String(incomingChar);
+    info = info + genere;
+  }
 }
--- prod/arduino/PortSerial.js
+++ prod/arduino/PortSerial.js
@@ -2,21 +2,50 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var SerialPort = require("serialport");
 var Events = require("events");
-var ReadLine = require("readline");
 var threshold = 100;
 var maxPotentiometer = 1024;
 var minPotentiometer = 0;
 var PortSerial = (function () {
     function PortSerial(serialPort, options) {
+        var _this = this;
         this.valueStocked = 0;
         this.myUsb = new SerialPort(serialPort, options);
         this.data = new Events();
+        this.isReceivedFromArduino = true;
+        this.sendData("\n");
         this.myUsb.on("data", function (value) {
-            ReadLine.clearLine(process.stdout, 0);
-            ReadLine.cursorTo(process.stdout, 0, null);
-            process.stdout.write(value);
+            var dataToSend = "bonjours";
+            if (value === "recu") {
+                _this.isReceivedFromArduino = true;
+            }
+            if (_this.isReceivedFromArduino) {
+                _this.isReceivedFromArduino = false;
+                _this.sendData(dataToSend + "\n");
+            }
+            console.log(value);
+            var valueParsed = "null";
+            try {
+                valueParsed = JSON.parse(value);
+            }
+            catch (e) {
+                valueParsed = e;
+            }
+            console.log(valueParsed);
         });
     }
+    PortSerial.prototype.sendData = function (dataToSend) {
+        var _this = this;
+        this.myUsb.write(new Buffer(dataToSend, 'ascii'), function (err) {
+            if (err) {
+                console.log("r'envois d'initialisation");
+                setTimeout(function () {
+                    _this.sendData("\n");
+                }, 3000);
+                return console.log('Error on write: ', err.message);
+            }
+            console.log('message envoyé');
+        });
+    };
     return PortSerial;
 }());
 exports.default = PortSerial;
