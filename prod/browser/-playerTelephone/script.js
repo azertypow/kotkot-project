84e937ee66a45be503eb3492b807660b18b9db91
mathildebuenerd <mathilde.buenerd@gmail.com>
2017-05-16 11:31:10 +0200

    animation du random des lois - le joueur peut cliquer dessus pour les générer

--- prod/browser/-playerTelephone/script.js
+++ prod/browser/-playerTelephone/script.js
@@ -2,26 +2,79 @@
  * Created by mathi on 15/05/2017.
  */
 
-// generateLaws(3);
 
-function generateLaws(nbCards) {
+var messages = {
+    "a":"Tire trois lois au hasard",
+    "b":"Choisis-en deux à envoyer à l'autre joueur"
+};
+
+var displayedLaws = 0;
+
+displayMessage(messages.a);
+
+createLaws(3);
+
+
+// Ajoute les listeners
+var aLaw = document.getElementsByClassName('law');
+for (var i=0; i<aLaw.length; i++) {
+    aLaw[i].addEventListener('click', generateLaw);
+}
+
+// crée les 3 emplacements pour les lois
+function createLaws(nbCards) {
+
+    var lawsBlock = document.getElementById('laws');
+    for (var i=0; i<nbCards; i++) {
+        var oneLaw = document.createElement("div");
+        oneLaw.setAttribute("class", "law");
+        oneLaw.setAttribute("id", "law-" + i);
+        lawsBlock.appendChild(oneLaw);
+    }
+}
+
+// choisit une loi au hasard quand on clique sur un carré
+function generateLaw(e) {
+
+    var thisLaw = document.getElementById(e.target.id);
 
     var lawType = ["Humaniste", "Progressiste"];
-    var lawsBlock = document.getElementById('laws');
+    var index = Math.floor(Math.random()*lawType.length);
+    var cardType = lawType[index];
+    var oneLaw = thisLaw;
+    oneLaw.className += " " + cardType.toLowerCase();
+    var lawContent = document.createElement("p");
+    lawContent.textContent = "Loi " + cardType;
+    oneLaw.appendChild(lawContent);
 
-    lawsBlock.innerHTML = "";
+    thisLaw.removeEventListener('click', generateLaw);
 
-    for (var i=0; i<nbCards; i++) {
-        var index = Math.floor(Math.random()*lawType.length);
-        var cardType = lawType[index];
-        var oneLaw = document.createElement("div");
-        oneLaw.setAttribute("class", "law " + cardType.toLowerCase());
-        lawsBlock.appendChild(oneLaw);
-        var lawContent = document.createElement("p");
-        lawContent.textContent = "Loi " + cardType;
-        oneLaw.appendChild(lawContent);
+    displayedLaws++;
+
+    // quand trois lois sont affichées, on a la possibilité de les choisir
+    if (displayedLaws === 3) {
+        displayMessage(messages.b);
+        var laws = document.getElementsByClassName("law");
+        for (var i=0; i<laws.length; i++) {
+            laws[i].addEventListener('click', selectTwoLaws);
+        }
+
     }
 
+}
 
+function selectTwoLaws(e) {
+
+    var thisLaw = document.getElementById(e.target.id);
+    thisLaw.className += " selectedLaw";
 
 }
+
+function displayMessage(message) {
+
+    var blocMessage = document.getElementById('message');
+    blocMessage.textContent = message;
+
+}
+
+
