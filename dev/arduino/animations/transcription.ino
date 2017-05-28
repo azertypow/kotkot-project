fa2922d48d7a80569dc733f7c62f9973cdcfec69
mathildebuenerd <mathilde.buenerd@gmail.com>
2017-05-28 16:13:38 +0200

    ajout des animations de leds avec arduino

---
+++ dev/arduino/animations/transcription.ino
@@ -0,0 +1,41 @@
+int transcriptionBande(char value) {
+
+  if (value == 'j') {
+    return 0;
+  } else if (value == 'k') {
+    return 1;
+  } else if (value == 'l') {
+    return 2;
+  } else if (value == 'm') {
+    return 3;
+  } else if (value == 'n') {
+    return 4;
+  } else if (value == 'o') {
+    return 5;
+  } else if (value == 'p') {
+    return 6;
+  } else if (value == 'q') {
+    return 7;
+  }
+  
+}
+
+int transcriptionColor(char color) {
+
+  if (color == 'a') {
+    return 0;
+  } else if (color == 'b') {
+    return 1;
+  } else if (color == 'c') {
+    return 2;
+  } else if (color == 'd') {
+    return 3;
+  } else if (color == 'e') {
+    return 4;
+  } else if (color == 'f') {
+    return 5;
+  } else if (color == 'g') {
+    return 6;
+  }
+  
+}
