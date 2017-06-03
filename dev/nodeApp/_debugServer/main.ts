9ca2b56c7616a0d5a3a0e9bbe0727fd7056ee86e
azertypow <azertypow@gmail.com>
2017-06-03 14:56:46 +0200

    mise en place server de debugage pour windows

---
+++ dev/nodeApp/_debugServer/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by azertypow on 03/06/2017.
+ */
+
+import AppServer from "../appServer"
+
+// lancer le serveur et la connection des joueur socket
+AppServer.run(1337);
---
+++ prod/nodeApp/_debugServer/main.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const appServer_1 = require("../appServer");
+appServer_1.default.run(1337);
