33bf7463fcb629752b66fd4303e27875b5aba0d2
mathildebuenerd <mathilde.buenerd@gmail.com>
2017-06-03 17:33:56 +0200

    ajout d'un fonction pour importer le json

---
+++ prod/browser/-playerTelephone/importJSON.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mathi on 03/06/2017.
+ */
+
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200 || httpRequest.status === 0) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
+
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+fetchJSONFile('../../../data/voice.json', function(data){
+    // do something with your data
+    console.log(data);
+});
